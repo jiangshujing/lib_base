89b5e507a0c76522c7b5b798ab510f9d9afb1956
jiangshujing <jiangshujing@teddymobile.cn>
2019-06-05 10:59:42 +0800

    添加方法

---
+++ src/main/java/com/example/jiangshujing/lib_base/TestBase.java
@@ -0,0 +1,15 @@
+package com.example.jiangshujing.lib_base;
+
+public class TestBase {
+
+    static String name;
+
+    public static String get() {
+        return name;
+    }
+
+
+    public static void set(String name) {
+        TestBase.name = name;
+    }
+}
